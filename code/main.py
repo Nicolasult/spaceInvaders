6c900aa4a06366cb61b9250689e215860cb50631
nicolasult <nicolas.ultrillo@gmail.com>
2025-06-03 19:29:41 +0200

    Add player and movement via input

--- code/main.py
+++ code/main.py
@@ -1,19 +1,32 @@
 import pygame, sys
+from player import Player
 
-pygame.init()
+class Game:
+    def __init__(self):
+        player_sprite = Player((screen_width / 2, screen_height ))
+        self.player = pygame.sprite.GroupSingle(player_sprite)
 
-screen_width = 600
-screen_height = 600
-screen = pygame.display.set_mode((screen_width, screen_height))
-clock  = pygame.time.Clock()
+    def run(self):
+        self.player.update()
+        self.player.draw(screen)
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-    
-    screen.fill((30, 30, 30))
+if __name__ == "__main__":
+    pygame.init()
 
-    pygame.display.flip()
-    clock.tick(60)
+    screen_width = 600
+    screen_height = 600
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    clock  = pygame.time.Clock()
+    game = Game()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        
+        screen.fill((30, 30, 30))
+        game.run()
+
+        pygame.display.flip()
+        clock.tick(60)
