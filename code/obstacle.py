da22b7d60745202209f8256d3d98d0bd51391ac5
nicolasult <nicolas.ultrillo@gmail.com>
2025-06-07 12:39:37 +0200

    Draw obstacle

--- code/obstacle.py
+++ code/obstacle.py
@@ -2,7 +2,7 @@ import pygame
 
 class Block(pygame.sprite.Sprite):
     def __init__(self, size, color, x, y):
-        super().__init__
+        super().__init__()
         self.image = pygame.Surface((size, size))
         self.image.fill(color)
         self.rect = self.image.get_rect(topleft = (x, y))
